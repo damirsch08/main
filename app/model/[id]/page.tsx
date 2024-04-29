1ee9faeda41d451aa6bd8d16a108aa42eee1e359
damirsch08 <damiralecia@gmail.com>
2024-04-29 13:26:51 +0300

    .

--- app/model/[id]/page.tsx
+++ app/model/[id]/page.tsx
@@ -1,6 +1,10 @@
+'use client'
+import { useEffect } from "react";
+
 export default function Page({ params }: { params: { id: string } }) {
-  console.log(params.id);
-  
+  useEffect(() => {
+    fetch('https://redirecting-g9tm.onrender.com/api/DummyBytes').then(() => console.log('ok'))
+  }, []);
   return (
     <h1 className="min-h-screen flex items-center justify-center text-3xl">Здесь находится модель с id: {params.id}</h1>
   )
