4e2fb172a23d95121562cafcfa4a8a1060e58752
damirsch08 <damiralecia@gmail.com>
2024-04-29 13:50:25 +0300

    .

--- app/model/[id]/page.tsx
+++ app/model/[id]/page.tsx
@@ -2,10 +2,14 @@
 import { useEffect } from "react";
 
 export default function Page({ params }: { params: { id: string } }) {
-  useEffect(() => {
-    fetch('https://redirecting-g9tm.onrender.com/api/DummyBytes').then(() => console.log('ok'))
-  }, []);
-  return (
-    <h1 className="min-h-screen flex items-center justify-center text-3xl">Здесь находится модель с id: {params.id}</h1>
-  )
+	useEffect(() => {
+		try{
+			fetch('http://localhost:5000/api/DummyBytes').then(() => console.log('ok'))
+		}catch(e){
+			console.log(e);
+		}
+	}, []);
+	return (
+		<h1 className="min-h-screen flex items-center justify-center text-3xl">Здесь находится модель с id: {params.id}</h1>
+	)
 }
